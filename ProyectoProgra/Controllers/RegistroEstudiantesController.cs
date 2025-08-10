8cef0c77f0cbdd0646afe02e81c2a99fcab6842d
Francis Scolly <FrancisScolly04@gmail.com>
2025-08-10 15:15:30 -0600

    cambios y mejoras

---
+++ ProyectoProgra/Controllers/RegistroEstudiantesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TuProyecto.Models;
+using System.Collections.Generic;
+
+namespace TuProyecto.Controllers
+{
+    public class RegistroEstudiantesController : Controller
+    {
+        // Lista simulando una base de datos (puedes reemplazar por EF Core)
+        private static List<Estudiante> estudiantes = new List<Estudiante>();
+
+        public IActionResult RegistroEstudiantes()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult RegistroEstudiantes(Estudiante estudiante)
+        {
+            if (ModelState.IsValid)
+            {
+                estudiante.Id = estudiantes.Count + 1;
+                estudiantes.Add(estudiante);
+                return RedirectToAction("ListaEstudiante", "RegistroEstudiantes");
+            }
+            return View(estudiante);
+        }
+
+        public IActionResult ListaEstudiante()
+        {
+            return View(estudiantes);
+        }
+    }
+}
---
+++ ProyectoProgra/Models/Estudiante.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TuProyecto.Models
+{
+    public class Estudiante
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(50)]
+        public string Nombre { get; set; }
+
+        [Required(ErrorMessage = "El apellido es obligatorio")]
+        [StringLength(50)]
+        public string Apellido { get; set; }
+
+        [Required(ErrorMessage = "La edad es obligatoria")]
+        [Range(1, 120, ErrorMessage = "La edad debe ser un valor entre 1 y 120")]
+        public int Edad { get; set; }
+
+        [Required(ErrorMessage = "El correo es obligatorio")]
+        [EmailAddress(ErrorMessage = "Formato de correo inválido")]
+        public string Correo { get; set; }
+
+        public DateTime FechaRegistro { get; set; } = DateTime.Now;
+    }
+}
